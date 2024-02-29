e35877027daab3cd45255e370b18aa59ca794423
VikBer <111386131+VikBer@users.noreply.github.com>
2024-03-01 01:17:09 +0300

    Add files via upload
    init

---
+++ src/test/java/tests/MainPageTests.java
@@ -0,0 +1,78 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import pages.SteamPage;
+
+@Owner("Victor Berezovsky")
+@Epic("UI")
+public class MainPageTests extends TestBase {
+    SteamPage steamPage = new SteamPage();
+    Faker faker = new Faker();
+
+    String game = "Cyberpunk",
+            player = faker.name().firstName();
+
+    @DisplayName("Проверка содержимого хэдер меню")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test
+    void headerMenuContentTest() {
+        steamPage.openPage()
+                .headerMenuContent();
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Проверка результата поисковой выдачи")
+    @Test
+    void searchFieldTest() {
+        steamPage.openPage()
+                .searchFieldEnterKey(game)
+                .searchFieldResult(game);
+    }
+
+    @Severity(SeverityLevel.BLOCKER)
+    @DisplayName("Проверка наличия поля поиска при выборе категории в меню Поддержка")
+    @Test
+    void searchFieldAppearsInSupportMenuTest() {
+        steamPage.openPage()
+                .supportButtonClick()
+                .gamesAndProgramsClick()
+                .verifySearchSupportInputVisible();
+
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Проверка результаты выдачи поиска по Игрокам")
+    @Test
+    void friendsSearchTest() {
+        steamPage.openPage()
+                .communityButtonClick()
+                .searchFriendsInputEnterKey(player)
+                .verifyFriendsSearchResult(player);
+
+
+    }
+
+    @DisplayName("Проверка корректного отображения текста при смене языка")
+    @Severity(SeverityLevel.CRITICAL)
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Italiano, Installa Steam",
+            "Deutsch, Steam installieren",
+            "Русский, Установить Steam"
+    })
+    void correctLanguageDisplayTest2(String language, String expectedResult) {
+        steamPage.openPage()
+                .clickLanguageButton()
+                .setLanguage(language)
+                .verifyLanguageContentResult(expectedResult);
+
+    }
+}
